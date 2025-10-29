4cda6a8ecd39a9330925ebb85d3ea779796e7023
Matias, Solis Schneeberger <matias.solis.sch@gmail.com>
2025-10-29 16:46:42 -0300

    agregando atributos, constructor y accesores de Socio

--- Socio.java
+++ Socio.java
@@ -1,3 +1,55 @@
-public class Socio {
+import java.util.ArrayList;
 
+public abstract class Socio {
+    private int dniSocio;
+    private String nombre;
+    private int diasPrestamo;
+    private ArrayList<Prestamo> prestamos;
+
+    Socio(int p_dniSocio, String p_nombre, int p_diasPrestamo) {
+        this.setDniSocio(p_dniSocio);
+        this.setNombre(p_nombre);
+        this.setDiasPrestamo(p_diasPrestamo);
+        this.setPrestamos(new ArrayList<>());
+    }
+
+    public int getDniSocio() {
+        return dniSocio;
+    }
+
+    private void setDniSocio(int p_dniSocio) {
+        this.dniSocio = p_dniSocio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    private void setNombre(String p_nombre) {
+        this.nombre = p_nombre;
+    }
+
+    public int getDiasPrestamo() {
+        return diasPrestamo;
+    }
+
+    private void setDiasPrestamo(int p_diasPrestamo) {
+        this.diasPrestamo = p_diasPrestamo;
+    }
+
+    public ArrayList<Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    private void setPrestamos(ArrayList<Prestamo> p_prestamos) {
+        this.prestamos = p_prestamos;
+    }
+
+    public boolean agregarPrestamo(Prestamo p_prestamo) {
+        return this.getPrestamos().add(p_prestamo);
+    }
+
+    public boolean quitarPrestamo(Prestamo p_prestamo) {
+        return this.getPrestamos().remove(p_prestamo);
+    }
 }
