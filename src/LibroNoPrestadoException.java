6e333680215b7f1bd5add65241e4f266509cd104
Gabriel66Elias <elias144fer@gmail.com>
2025-11-09 20:53:41 -0300

    Update LibroNoPrestadoException.java

--- src/LibroNoPrestadoException.java
+++ src/LibroNoPrestadoException.java
@@ -1,5 +1,11 @@
+/**
+ * Clase que crea la Exception de tipo "LibroNoPrestadoException" con mensaje descriptivo "El libro se encuentra en biblioteca".
+ * @author Fernandez Gabriel Elías
+ * @version 04/11/2025
+ */
 public class LibroNoPrestadoException extends Exception{
     public LibroNoPrestadoException(String mensaje){
         super(mensaje);
     }
+
 }
